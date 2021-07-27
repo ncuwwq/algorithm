1457781c69ffeeafccced0ee477bc7d6c2e9ab49
WeiQqiang <997626697@qq.com>
2021-07-27 09:21:18 +0800

    并查集

---
+++ python/disjointSet.py
@@ -0,0 +1,137 @@
+#并查集模板
+from typing import List
+
+class UnionFind:
+    def __init__(self):
+        """
+        记录每个节点的父节点
+        """
+        self.father = {}
+
+    def find(self, x):
+        """
+        查找根节点
+        路径压缩
+        """
+        root = x
+
+        while self.father[root] != None:
+            root = self.father[root]
+
+        # 路径压缩
+        while x != root:
+            original_father = self.father[x]
+            self.father[x] = root
+            x = original_father
+
+        return root
+
+    def merge(self, x, y, val):
+        """
+        合并两个节点
+        """
+        root_x, root_y = self.find(x), self.find(y)
+
+        if root_x != root_y:
+            self.father[root_x] = root_y
+
+    def is_connected(self, x, y):
+        """
+        判断两节点是否相连
+        """
+        return self.find(x) == self.find(y)
+
+    def add(self, x):
+        """
+        添加新节点
+        """
+        if x not in self.father:
+            self.father[x] = None
+
+#案例
+# 给你一个变量对数组 equations 和一个实数值数组 values 作为已知条件，其中 equations[i] = [Ai, Bi] 和 values[i] 共同表示等式 Ai / Bi = values[i] 。
+# 每个 Ai 或 Bi 是一个表示单个变量的字符串。
+# 另有一些以数组 queries 表示的问题，其中 queries[j] = [Cj, Dj] 表示第 j 个问题，请你根据已知条件找出 Cj / Dj = ? 的结果作为答案。
+# 返回 所有问题的答案 。如果存在某个无法确定的答案，则用 -1.0 替代这个答案。如果问题中出现了给定的已知条件中没有出现的字符串，也需要用 -1.0 替代这个答案。
+#
+# 输入：equations = [["a","b"],["b","c"]], values = [2.0,3.0], queries = [["a","c"],["b","a"],["a","e"],["a","a"],["x","x"]]
+# 输出：[6.00000,0.50000,-1.00000,1.00000,-1.00000]
+# 解释：
+# 条件：a / b = 2.0, b / c = 3.0
+# 问题：a / c = ?, b / a = ?, a / e = ?, a / a = ?, x / x = ?
+# 结果：[6.0, 0.5, -1.0, 1.0, -1.0 ]
+
+class UnionFind:
+    def __init__(self):
+        """
+        记录每个节点的父节点
+        记录每个节点到根节点的权重
+        """
+        self.father = {}
+        self.value = {}
+
+    def find(self, x):
+        """
+        查找根节点
+        路径压缩
+        更新权重
+        """
+        root = x
+        # 节点更新权重的时候要放大的倍数
+        base = 1
+        while self.father[root] != None:
+            root = self.father[root]
+            base *= self.value[root]
+
+        while x != root:
+            original_father = self.father[x]
+            ##### 离根节点越远，放大的倍数越高
+            self.value[x] *= base
+            base /= self.value[original_father]
+            #####
+            self.father[x] = root
+            x = original_father
+
+        return root
+
+    def merge(self, x, y, val):
+        """
+        合并两个节点
+        """
+        root_x, root_y = self.find(x), self.find(y)
+
+        if root_x != root_y:
+            self.father[root_x] = root_y
+            ##### 四边形法则更新根节点的权重
+            self.value[root_x] = self.value[y] * val / self.value[x]
+
+    def is_connected(self, x, y):
+        """
+        两节点是否相连
+        """
+        return x in self.value and y in self.value and self.find(x) == self.find(y)
+
+    def add(self, x):
+        """
+        添加新节点，初始化权重为1.0
+        """
+        if x not in self.father:
+            self.father[x] = None
+            self.value[x] = 1.0
+
+
+class Solution:
+    def calcEquation(self, equations: List[List[str]], values: List[float], queries: List[List[str]]) -> List[float]:
+        uf = UnionFind()
+        for (a, b), val in zip(equations, values):
+            uf.add(a)
+            uf.add(b)
+            uf.merge(a, b, val)
+
+        res = [-1.0] * len(queries)
+
+        for i, (a, b) in enumerate(queries):
+            if uf.is_connected(a, b):
+                res[i] = uf.value[a] / uf.value[b]
+        return res
+
